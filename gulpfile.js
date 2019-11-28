1967628e647504f4632fe41b98aafd076599f398
kostatine <kostatine@gmail.com>
2019-11-28 15:14:01 +0200

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ cssmin = require('gulp-cssmin');
 
 
 gulp.task('sass', function(){
-    return gulp.src('app/scss/style.scss')
+    return gulp.src('app/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(rename({suffix: ".min"}))
     .pipe(autoprefixer({
@@ -36,7 +36,7 @@ gulp.task('script', function(){
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/slick-carousel/slick/slick.js'        
     ])
-    .pipe(concat('libs/min.js'))
+    .pipe(concat('libs.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('app/js'))
 });
@@ -60,7 +60,7 @@ gulp.task('script', function(){
     });
 
     gulp.task('watch', function(){
-        gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+        gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
         gulp.watch('app/*.html', gulp.parallel('html'))
         gulp.watch('app/js/*.js',gulp.parallel('js'))  
         });
