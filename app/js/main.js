1df3694a63fec6f9734d4a22aed9502257ca4906
kostatine <kostatine@gmail.com>
2019-12-09 23:49:29 +0200

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+      });
+     
+      $('.product-slider__inner').slick({
+          dots:true,
+          arrows:false,
+          slidesToShow:4,
+          slidesToScroll:4,
+      })
+
+    var mixer = mixitup('.products__inner-box')
 });
